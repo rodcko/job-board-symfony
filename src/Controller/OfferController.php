2c6cbe9072055afb12cbe931c67bf0aa8cd04602
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-06 17:31:19 -0600

    Mejor modo de tratar con los parametros de las URL

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OfferController extends AbstractController
@@ -24,13 +25,9 @@ class OfferController extends AbstractController
     }
 
     #[Route('/job_offer/{id}/apply', name: 'offer_apply')]
-    public function apply(int $id, EntityManagerInterface $entityManager)
+    public function apply(JobOffer $offer)
     {
-
-        $offer = $entityManager->getRepository(JobOffer::class)
-            ->find($id);
-
-        return $this->render('offer/apply.html.twig', [
+        return $this->render('offer/apply', [
             'offer' => $offer,
         ]);
     }
